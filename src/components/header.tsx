c3284bcc4c11b72905c9dca487adac49a2852eba
Tommi Mäki <bgp010@myy.haaga-helia.fi>
2024-11-22 19:05:51 +0200

    initial setup, header

---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <div className="w-full border .border-solid flex justify-between">
+      Header
+      <ul className="flex w-50% justify-between">
+        <li>
+          <a href="">lorem lorem</a>{" "}
+        </li>
+        <li>lorem lorem </li>
+        <li>lorem lorem </li>
+        <li>lorem lorem </li>
+      </ul>
+    </div>
+  );
+}
